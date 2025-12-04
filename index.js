5de3ac4ca07826af44701576bec7d93e945eaa27
Elizabeth Bey <ebey04@gmail.com>
2025-12-04 17:30:13 -0500

    feat: create getKnightMooves() helper function

--- index.js
+++ index.js
@@ -33,3 +33,24 @@ function buildPath(goal, parent) {
     }
     return path;
 }
+
+function getKnightMoves([startRow, startCol]) {
+    const boardSize = 8; // For an 8x8 chessboard
+    const knightMoves = [
+        [2, 1], [1, 2], [-1, 2], [-2, 1],
+        [-2, -1], [-1, -2], [1, -2], [2, -1]
+    ];
+    const availableMoves = [];
+
+    for (const move of knightMoves) {
+        const newRow = startRow + move[0];
+        const newCol = startCol + move[1];
+
+        // Check if the new position is within the board boundaries
+        if (newRow >= 0 && newRow < boardSize && newCol >= 0 && newCol < boardSize) {
+        availableMoves.push([newRow, newCol]);
+        }
+    }
+
+    return availableMoves;
+}
