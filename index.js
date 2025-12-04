2dbe70b80cb3606e9666aff9c4d8da5ca7a56ad0
Elizabeth Bey <ebey04@gmail.com>
2025-12-04 12:50:18 -0500

    feat: build base files. add main function skeleton for knightMoves()

---
+++ index.js
@@ -0,0 +1,24 @@
+function knightMoves(start, goal) {
+    const queue = [];
+    const visited = new Set();
+    const parent = new Map();
+
+    queue.push(start);
+    visited.add(start); 
+
+    while (queue.length > 0) {
+        const node = queue.shift(); 
+
+        if (JSON.stringify(node) == JSON.stringify(goal)) {
+            return buildPath(goal, parent)
+        }
+
+        for ( let neighbor of getKnightMoves(node)) {
+            if (!visited.has(JSON.stringify(neighbor))) {
+                visited.add(JSON.stringify(neighbor));
+                parent.set(JSON.stringify(neighbor), JSON.stringify(node));
+                queue.push(neighbor)
+            }
+        }
+    }
+}
